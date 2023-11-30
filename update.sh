530d390a649000f34b8fdeff5cb5e86627f02ce2
0187773933 <collincerbus@student.olympic.edu>
2023-11-30 15:03:31 -0500

    34

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@ GITHUB_REPO="https://github.com/0187773933/FireC2Server"
 
 # Check for a lock file
 if [ -f "$LOCK_FILE" ]; then
-    echo "Another build is in progress. Exiting."
-    exit 1
+	echo "Another build is in progress. Exiting."
+	exit 1
 fi
 
 # Set a lock file
@@ -20,18 +20,18 @@ REMOTE_HASH=$(git ls-remote https://github.com/0187773933/FireC2Server.git HEAD 
 
 # Check if the hash file exists and read the last stored hash
 if [ -f "$HASH_FILE" ]; then
-    STORED_HASH=$(cat "$HASH_FILE")
+	STORED_HASH=$(cat "$HASH_FILE")
 else
-    STORED_HASH=""
+	STORED_HASH=""
 fi
 
 # Compare the hashes
 if [ "$REMOTE_HASH" != "$STORED_HASH" ]; then
-    echo "New updates available. Updating and restarting the container."
-    echo "$REMOTE_HASH" > "$HASH_FILE"
-    sudo ./dockerRun.sh
+	echo "New updates available. Updating and restarting the container."
+	echo "$REMOTE_HASH" > "$HASH_FILE"
+	sudo ./dockerRestart.sh
 else
-    echo "No updates available."
+	echo "No updates available."
 fi
 
 # Remove the lock file
