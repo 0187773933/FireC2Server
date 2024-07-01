b00f754f097324d0470eb59e0c7e94619f641731
0187773933 <collincerbus@student.olympic.edu>
2024-07-01 16:11:27 -0400

    103

--- gitPushUpdate.sh
+++ gitPushUpdate.sh
@@ -4,7 +4,7 @@ FILE="./v1/server/html/login.html"
 NEW_LINE="\t\t\t\t\t<form id=\"form-login\" action=\"/ok???\" onSubmit=\"return on_submit();\" method=\"post\">"
 awk -v n=17 -v s="$NEW_LINE" '(NR == n) {$0 = s} 1' $FILE > temp.txt
 mv temp.txt $FILE
-sed -i '' '3s/.*/go 1.18/' go.mod # mac osx
+sed -i '' -e '3s/.*/go 1.18/' -e '/toolchain go/d' go.mod # mac osx
 
 function is_int() { return $(test "$@" -eq "$@" > /dev/null 2>&1); }
 ssh-add -D
@@ -41,4 +41,4 @@ git remote add origin git@github.com:0187773933/FireC2Server.git
 git push origin --tags
 git push origin master
 
-sed -i '' '3s/.*/go 1.22.0/' go.mod # mac osx
+sed -i '' -e '3s/.*/go 1.22.0/'  go.mod
